a57a537cefdd4284867316c013c51758c2b2cc91
Moritz Huebner <email@moritz-huebner.de>
2018-06-26 14:23:23 +1000

    Extracted waveform wrapper functions into separate file

---
+++ scripts/waveform_functions.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+import gwmemory
+
+
+def time_domain_nr_sur_waveform_without_memory(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+                                               luminosity_distance,
+                                               inc, pol, LMax, **kwargs):
+    memory_generator = gwmemory.waveforms.Surrogate(q=mass_ratio,
+                                                    name='',
+                                                    MTot=total_mass,
+                                                    S1=np.array([s11, s12, s13]),
+                                                    S2=np.array([s21, s22, s23]),
+                                                    LMax=LMax,
+                                                    times=times,
+                                                    distance=luminosity_distance
+                                                    )
+    h_oscillatory, _ = memory_generator.time_domain_oscillatory(times=times, inc=inc, pol=pol)
+    return h_oscillatory
+
+
+def time_domain_nr_sur_waveform_with_memory(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23,
+                                            luminosity_distance,
+                                            inc, pol, LMax, **kwargs):
+    memory_generator = gwmemory.waveforms.Surrogate(q=mass_ratio,
+                                                    name='',
+                                                    MTot=total_mass,
+                                                    S1=np.array([s11, s12, s13]),
+                                                    S2=np.array([s21, s22, s23]),
+                                                    LMax=LMax,
+                                                    times=times,
+                                                    distance=luminosity_distance
+                                                    )
+    h_oscillatory, _ = memory_generator.time_domain_oscillatory(times=times, inc=inc, pol=pol)
+    h_memory, _ = memory_generator.time_domain_memory(inc=inc, pol=pol)
+    res = dict()
+    for mode in h_memory:
+        res[mode] = h_memory[mode] + h_oscillatory[mode]
+    return res
+
+
+def time_domain_nr_sur_memory_waveform(times, mass_ratio, total_mass, s11, s12, s13, s21, s22, s23, luminosity_distance,
+                                       inc, pol, LMax, **kwargs):
+    memory_generator = gwmemory.waveforms.Surrogate(q=mass_ratio,
+                                                    name='',
+                                                    MTot=total_mass,
+                                                    S1=np.array([s11, s12, s13]),
+                                                    S2=np.array([s21, s22, s23]),
+                                                    LMax=LMax,
+                                                    times=times,
+                                                    distance=luminosity_distance
+                                                    )
+    h_memory, _ = memory_generator.time_domain_memory(inc=inc, pol=pol)
+    return h_memory
