95ba41bcb176b49b45d5a5f0f78ebd652dab980c
MoritzThomasHuebner <email@moritz-huebner.de>
2018-08-15 14:55:58 +1000

    Reverted some of the recent changes

--- gwmemory/waveforms.py
+++ gwmemory/waveforms.py
@@ -417,7 +417,7 @@ class Approximant(MemoryGenerator):
         self._check_prececssion()
 
         MemoryGenerator.__init__(self, name=name, h_lm=dict(), times=times, distance=distance)
-        self.h_lm = self.time_domain_oscillatory(inc=inc, phase=phase)
+        self.h_lm, self.times = self.time_domain_oscillatory(inc=inc, phase=phase)
 
 
     @property
@@ -532,37 +532,47 @@ class Approximant(MemoryGenerator):
         times: np.array
             Times on which waveform is evaluated.
         """
-        if modes is None:
-            modes = self.available_modes
+        if self.h_lm is None:
+            if modes is None:
+                modes = self.available_modes
+            else:
+                modes = modes
+
+            if not set(modes).issubset(self.available_modes):
+                print('Requested {} unavailable modes'.format(' '.join(set(modes).difference(self.available_modes))))
+                modes = list(set(modes).union(self.available_modes))
+                print('Using modes {}'.format(' '.join(modes)))
+
+            fmin = 20
+            fRef = 20
+            theta = 0.0
+            phi = 0.0
+            longAscNodes = 0.0
+            eccentricity = 0.0
+            meanPerAno = 0.0
+            WFdict = lal.CreateDict()
+
+            hplus, hcross = lalsim.SimInspiralChooseTDWaveform(
+                self.m1_SI, self.m2_SI, self.S1[0], self.S1[1], self.S1[2], self.S2[0], self.S2[1], self.S2[2],
+                self.distance_SI, theta, phi, longAscNodes, eccentricity, meanPerAno, self.delta_t, fmin, fRef,
+                WFdict, self.approximant)
+
+            h = hplus.data.data - 1j * hcross.data.data
+            h_22 = h / harmonics.sYlm(-2, 2, 2, theta, phi)
+
+            times = np.linspace(0, self.delta_t * len(h), len(h))
+            times -= times[np.argmax(abs(h_22))]
+
+            h_lm = {(2, 2): h_22, (2, -2): np.conjugate(h_22)}
+
         else:
-            modes = modes
-        if not set(modes).issubset(self.available_modes):
-            print('Requested {} unavailable modes'.format(' '.join(set(modes).difference(self.available_modes))))
-            modes = list(set(modes).union(self.available_modes))
-            print('Using modes {}'.format(' '.join(modes)))
-
-        fmin = 20
-        fRef = 20
-        theta = 0.0
-        phi = 0.0
-        longAscNodes = 0.0
-        eccentricity = 0.0
-        meanPerAno = 0.0
-        WFdict = lal.CreateDict()
-
-        hplus, hcross = lalsim.SimInspiralChooseTDWaveform(
-            self.m1_SI, self.m2_SI, self.S1[0], self.S1[1], self.S1[2], self.S2[0], self.S2[1], self.S2[2],
-            self.distance_SI, theta, phi, longAscNodes, eccentricity, meanPerAno, self.delta_t, fmin, fRef,
-            WFdict, self.approximant)
-
-        h = hplus.data.data - 1j * hcross.data.data
-        h_22 = h / harmonics.sYlm(-2, 2, 2, theta, phi)
-        h_lm = {(2, 2): h_22, (2, -2): np.conjugate(h_22)}
+            h_lm = self.h_lm
+            times = self.times
 
         if inc is None or phase is None:
-            return h_lm
+            return h_lm, times
         else:
-            return combine_modes(h_lm, inc, phase)
+            return combine_modes(h_lm=h_lm, inc=inc, phase=phase), times
 
 
 class MWM(MemoryGenerator):
