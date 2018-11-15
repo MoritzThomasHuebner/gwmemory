a70b0b2dc6fe23a6e0d930d8dd6fec8c48dc4623
Moritz <email@moritz-huebner.de>
2018-11-15 13:12:04 +1100

    Did some changes to make it pip installable

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='gwmemory',
+setup(name='gwmemoritz',
       version='0.1.0',
       packages=['gwmemory'],
       package_dir={'gwmemory': 'gwmemory'},
