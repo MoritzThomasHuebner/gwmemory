e436294ed97b4cd59ab53362447b5a6f14c75ed9
Moritz <email@moritz-huebner.de>
2019-04-23 14:46:12 +1000

    fix

--- scripts/create_waveforms.py
+++ scripts/create_waveforms.py
@@ -11,7 +11,7 @@ l_max = 2
 inc = np.pi / 2
 phase = 0
 
-times = np.linspace(0, 16, 2048*2)
+times = np.linspace(0, 16, 2048*16)
 memory_generator = gwmemory.waveforms.HybridSurrogate(q=q,
                                                       total_mass=m_tot,
                                                       minimum_frequency=10,
