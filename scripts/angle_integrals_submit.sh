4cd27b50a1003eab8f6528e36fa787ee144942dd
Moritz <email@moritz-huebner.de>
2019-06-10 17:17:05 +1000

    Set up to get the correct angle integrals

--- scripts/angle_integrals_submit.sh
+++ scripts/angle_integrals_submit.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=real_event
+#SBATCH --job-name=angles
 #SBATCH --ntasks=1
 #SBATCH --time=1:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END --mail-user=email@moritz-huebner.de
 #
-#SBATCH --array=0-63
+#SBATCH --array=0-17
 ARGS=(-8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8)
 srun python angle_integrals.py ${ARGS[$SLURM_ARRAY_TASK_ID]}
