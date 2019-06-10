402043074f6280e0e09226316ed14fd3c579a454
Moritz <email@moritz-huebner.de>
2019-06-10 16:51:59 +1000

    Set up to get the correct angle integrals

--- scripts/angle_integrals_submit.sh
+++ scripts/angle_integrals_submit.sh
@@ -11,4 +11,4 @@ ARGS={-8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8}
 #
 #SBATCH --array=0-16
 
-srun python production_real_event.py $1 64 $ARGS{${SLURM_ARRAY_TASK_ID}}
+srun python angle_integrals.py $ARGS{${SLURM_ARRAY_TASK_ID}}
