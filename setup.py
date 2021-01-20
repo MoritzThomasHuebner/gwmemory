cda79dcde32bc66ad928a0580282aa7845436eff
Moritz Huebner <email@moritz-huebner.de>
2021-01-20 20:15:23 +1100

    New version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='gwmemory',
-      version='0.1.3',
+      version='0.2.0',
       packages=['gwmemory'],
       package_dir={'gwmemory': 'gwmemory'},
       package_data={'gwmemory': ['data/gamma_coefficients*.dat', 'data/*WEB.dat']},
