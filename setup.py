915abe2740a5a69347541cb96f931dfa9e6a1988
Moritz Huebner <email@moritz-huebner.de>
2021-01-22 17:21:19 +1100

    Fixed typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='gwmemory',
-      version='0.2.0',
+      version='0.2.0pip',
       packages=['gwmemory'],
       package_dir={'gwmemory': 'gwmemory'},
       package_data={'gwmemory': ['data/gamma_coefficients*.dat', 'data/*WEB.dat']},
